99a6508f9ae984dbcd2bf474297f2fa5c717719c
Musa Barighzaai <mbarighzaai@icloud.com>
2020-02-22 23:48:17 -0500

    init nodemon and babel

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
