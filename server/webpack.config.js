d61065f0939447a69105cc6aa0f7385f6587c3e4
Musa Barighzaai <mbarighzaai@icloud.com>
2020-02-23 00:34:33 -0500

    update webpack config

---
+++ server/webpack.config.js
@@ -0,0 +1,27 @@
+const nodeExternals = require('webpack-node-externals');
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
