0309ef0fb6fae1b7752cdd7986fd091b1e481159
Musa Barighzaai <mbarighzaai@icloud.com>
2020-02-23 01:57:46 -0500

    algo to find median of primes

---
+++ server/src/utils/eratosthenes.js
@@ -0,0 +1,31 @@
+// https://stackoverflow.com/a/15471749/3330490
+// Eratosthenes algorithm to find all primes under n
+
+export const eratosthenes = n => {
+  const array = [];
+  const output = [];
+  const upperLimit = Math.sqrt(n);
+
+  // Make an array from 2 to (n - 1)
+  for (var i = 0; i < n; i++) {
+    array.push(true);
+  }
+
+  // Remove multiples of primes starting from 2, 3, 5,...
+  for (var i = 2; i <= upperLimit; i++) {
+    if (array[i]) {
+      for (var j = i * i; j < n; j += i) {
+        array[j] = false;
+      }
+    }
+  }
+
+  // All array[i] set to true are primes
+  for (var i = 2; i < n; i++) {
+    if (array[i]) {
+      output.push(i);
+    }
+  }
+
+  return output;
+};
---
+++ server/src/utils/getMedian.js
@@ -0,0 +1,13 @@
+export const getMedian = nums => {
+  const numsLen = nums.length;
+  let median = 0;
+
+  // determine if length of nums is even or odd
+  if (numsLen % 2 === 0) {
+    median = [nums[numsLen / 2 - 1], nums[numsLen / 2 + 1]];
+  } else {
+    median = nums[(numsLen - 1) / 2];
+  }
+
+  return median;
+};
