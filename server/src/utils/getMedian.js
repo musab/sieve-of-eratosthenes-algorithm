a617f6074554031747674397cfa2cd6c631e2424
Musa Barighzaai <mbarighzaai@icloud.com>
2020-02-23 09:41:15 -0500

    fix median bug

--- server/src/utils/getMedian.js
+++ server/src/utils/getMedian.js
@@ -4,7 +4,7 @@ export const getMedian = nums => {
 
   // determine if length of nums is even or odd
   if (numsLen % 2 === 0) {
-    median = [nums[numsLen / 2 - 1], nums[numsLen / 2 + 1]];
+    median = [nums[numsLen / 2 - 1], nums[numsLen / 2]];
   } else {
     median = nums[(numsLen - 1) / 2];
   }
